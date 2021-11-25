059af6d2ce874e2c5cef32bf07dce8009a4c43a4
SafaKaraca <karacamuhammedsafa@gmail.com>
2021-11-26 00:06:59 +0300

    Instagram and Twitter logos/links are added in cards. New fonts are added.

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,14 @@ void main() {
     home: Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.red[900],
-        title: const Text('I am A Youtuber'),
+        title: const Text(
+          'I am a Youtuber',
+          style: TextStyle(fontFamily: 'Pacifico'),
+        ),
         centerTitle: true,
       ),
-      body: MainPage(),
-      backgroundColor: Colors.white,
+      body: const MainPage(),
+      backgroundColor: Colors.grey[200],
     ),
   ));
 }
@@ -27,24 +30,81 @@ class _MainPageState extends State<MainPage> {
     return SafeArea(
       child: Center(
         child: Column(children: [
-          SizedBox(
-            height: 40,
-          ),
-          Text(
-            'To see my youtube channel, please click the Youtube logo',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
-                color: Colors.red[900]),
-          ),
-          InkWell(
-            onTap: () => launch(
-                'https://www.youtube.com/channel/UCn7rIX4UXChWNBVp2wwKV_Q'),
-            child: const Image(
-              image: AssetImage('images/youtube.jpg'),
+          Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            margin: const EdgeInsets.fromLTRB(10, 20, 10, 20),
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Text(
+                'To see my channels, please click logos',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 20,
+                    fontFamily: 'Varela',
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.italic,
+                    color: Colors.red[700]),
+              ),
             ),
           ),
+          Row(children: [
+            Expanded(
+              child: Card(
+                margin: EdgeInsets.fromLTRB(10, 5, 0, 5),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(250),
+                ),
+                child: InkWell(
+                  onTap: () => launch(
+                      'https://www.youtube.com/channel/UCn7rIX4UXChWNBVp2wwKV_Q'),
+                  child: Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Image.asset(
+                      'images/youtube.png',
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Card(
+                margin: EdgeInsets.fromLTRB(10, 5, 0, 5),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(250),
+                ),
+                child: InkWell(
+                  onTap: () =>
+                      launch('https://www.instagram.com/safamuhammedkaraca/'),
+                  child: Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Image.asset(
+                      'images/Instagram.png',
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Card(
+                margin: EdgeInsets.fromLTRB(10, 5, 10, 5),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(250),
+                ),
+                child: InkWell(
+                  onTap: () =>
+                      launch('https://www.instagram.com/safamuhammedkaraca/'),
+                  child: Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Image.asset(
+                      'images/twitter.png',
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ]),
         ]),
       ),
     );
